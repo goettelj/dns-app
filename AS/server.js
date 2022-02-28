eaa4e0131e8975e7d0e56555214efb4f9c952828
Jay Goettelmann <jay@liberatedmatter.com>
2022-02-28 14:15:18 -0500

    Adds UDP responses to DNS Queries and Registrations.

--- AS/server.js
+++ AS/server.js
@@ -16,28 +16,35 @@ server.on("error", (err) => {
  * main handle section
  */
 server.on("message", async (msg, rinfo) => {
-  
   logMessage(msg, rinfo);
 
   const msgObj = dnsMessageToJson(msg);
 
-  switch (msgObj.msgType){
+  switch (msgObj.msgType) {
     case "REGISTRATION":
       saveRegistration(msg);
+      server.send("Successfully saved registration.", rinfo.port, rinfo.address, (err) => {
+        if (err) {
+          console.error(`Error sending DNS query response! ${err}`);
+        }
+      });
       break;
     case "QUERY":
       const searchString = `NAME=${msgObj.name}`;
-      const record = await findRecordInFile('data.txt', searchString);
-      
-      //TODO return response (UDP? TCP?)
-      console.log(`FOUND DNS RECORD!`);
-      console.log(`${record}`);
+      let record = await findRecordInFile("data.txt", searchString);
+
+      record = record || 'NOT_FOUND';
+      console.log(`Result of DNS query: ${record}`); 
+      server.send(record, rinfo.port, rinfo.address, (err) => {
+        if (err) {
+          console.error(`Error sending DNS query response! ${err}`);
+        }
+      });
       break;
     default:
       console.error("Invalid message received:");
       logMessage(msg, rinfo);
   }
-
 });
 
 server.on("listening", () => {
@@ -54,7 +61,6 @@ server.bind(PORT);
  * @param {*} msg - Registration text separated by newlines
  */
 const saveRegistration = async (msg) => {
-  
   const singleLineMsg = msg.toString().replaceAll(/\n/g, ",");
 
   try {
@@ -67,9 +73,9 @@ const saveRegistration = async (msg) => {
   } catch (err) {
     console.error(`Problem writing DNS entry to datafile: ${err}`);
   }
-}
+};
 
-function logMessage(msg, rinfo){
+function logMessage(msg, rinfo) {
   console.debug("================================");
   console.debug(new Date());
   console.debug("================================");
