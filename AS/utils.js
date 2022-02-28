7277be00dbed896e925a4bcd648317b01e34dce5
Jay Goettelmann <jay@liberatedmatter.com>
2022-02-28 14:18:37 -0500

    Updates msgToJson function to support line breaks and commas.

--- AS/utils.js
+++ AS/utils.js
@@ -12,28 +12,31 @@ const dnsMessageToJson = (msg) => {
   let response = {};
   msg = msg.toString();
 
+  // ensure the passed message end with a new line for regex matching
+  msg += "\n";
+
   // TYPE
-  const typeRegex = /TYPE=A\n/;
+  const typeRegex = /TYPE=A[\n|,]/;
   if (typeRegex.test(msg)) {
     response.type = "A";
   }
 
   // NAME
-  const nameRegex = /NAME=(.*)\n/;
+  const nameRegex = /NAME=([a-z\.]+)[\n|,]/;
   let match = msg.match(nameRegex);
   if (match) {
     response.name = match[1];
   }
 
   // VALUE
-  const valueRegex = /VALUE=(.*)\n/;
+  const valueRegex = /VALUE=([0-9\.]+)[\n|,]/;
   match = msg.match(valueRegex);
   if (match) {
     response.value = match[1];
   }
 
   // TTL
-  const ttlRegext = /TTL=(\d*)$/;
+  const ttlRegext = /TTL=(\d*)\n/;
   match = msg.match(ttlRegext);
   if (match) {
     response.ttl = parseInt(match[1]);
@@ -56,6 +59,8 @@ const getMessageType = (msgObj) => {
   } else if (keys.length == 4 && keys.every((key) => { return registrationKeys.includes(key)})) {
       return "REGISTRATION";
   } else {
+      console.log("Unknown with this object: ");
+      console.dir(msgObj);
       return "UNKNOWN";
   }
 };
@@ -85,7 +90,9 @@ const findRecordInFile = async (filePath, searchText) => {
                     resolve(line);
                 });
            } else {
-               console.debug(`${line} does not contain ${searchText}`);
+               return new Promise( (resolve, reject) => {
+                 reject("Did not find searchText in this line.");
+               });
            }
         }
 
